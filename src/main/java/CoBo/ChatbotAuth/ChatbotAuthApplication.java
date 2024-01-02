d392ea1db108c06f297140ba0404cd0e518b2a30
trust1204@gmail.com <trust1204@gmail.com>
2024-01-03 07:12:44 +0900

    first commit

---
+++ src/main/java/CoBo/ChatbotAuth/ChatbotAuthApplication.java
@@ -0,0 +1,13 @@
+package CoBo.ChatbotAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbotAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CoBo/ChatbotAuth/ChatbotAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package CoBo.ChatbotAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
