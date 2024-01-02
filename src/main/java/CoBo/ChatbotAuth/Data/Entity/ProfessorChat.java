499a5fb49559f29f02827ed85a905653da950fb2
trust1204@gmail.com <trust1204@gmail.com>
2024-01-03 07:59:57 +0900

    [feat] add ProfessorChat Entity

---
+++ src/main/java/CoBo/ChatbotAuth/Data/Entity/ProfessorChat.java
@@ -0,0 +1,43 @@
+package CoBo.ChatbotAuth.Data.Entity;
+
+import CoBo.ChatbotAuth.Data.Entity.Embedded.ChatId;
+import CoBo.ChatbotAuth.Data.Enum.StateEnum;
+import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class ProfessorChat {
+
+    @EmbeddedId
+    private ChatId id;
+
+    @Column(length = 500)
+    private String question;
+
+    @Column(length = 500)
+    private String answer;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    private StateEnum state;
+
+    @Builder
+    public ProfessorChat(ChatId id, String question, String answer, LocalDateTime createdAt, LocalDateTime updatedAt, StateEnum state) {
+        this.id = id;
+        this.question = question;
+        this.answer = answer;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.state = state;
+    }
+}
