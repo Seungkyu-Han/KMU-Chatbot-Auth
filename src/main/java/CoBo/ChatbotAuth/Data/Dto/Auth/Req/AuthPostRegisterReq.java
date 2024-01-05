d16689bfb15db0b1803ba4b001f6184d598b2c57
trust1204@gmail.com <trust1204@gmail.com>
2024-01-05 11:34:33 +0900

    [feat] add PostRegister DTO

---
+++ src/main/java/CoBo/ChatbotAuth/Data/Dto/Auth/Req/AuthPostRegisterReq.java
@@ -0,0 +1,21 @@
+package CoBo.ChatbotAuth.Data.Dto.Auth.Req;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class AuthPostRegisterReq {
+
+    @NotNull
+    @Schema(description = "학번", example = "2021111222")
+    private Integer studentId;
+
+    @NotNull
+    @Schema(description = "이름", example = "킹승규")
+    private String name;
+
+    @NotNull
+    @Schema(description = "이메일", example = "trust1204@stu.kmu.ac.kr")
+    private String email;
+}
