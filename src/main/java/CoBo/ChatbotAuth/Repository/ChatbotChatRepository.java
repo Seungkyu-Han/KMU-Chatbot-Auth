4ff8d78b4414a3e90a03df3214b77e95025e03e9
trust1204@gmail.com <trust1204@gmail.com>
2024-01-13 15:59:23 +0900

    [!HOTFIX] remove unused Repository

--- src/main/java/CoBo/ChatbotAuth/Repository/ChatbotChatRepository.java
+++
@@ -1,8 +0,0 @@
-package CoBo.ChatbotAuth.Repository;
-
-import CoBo.ChatbotAuth.Data.Entity.ChatbotChat;
-import CoBo.ChatbotAuth.Data.Entity.Embedded.ChatId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatbotChatRepository extends JpaRepository<ChatbotChat, ChatId> {
-}
--- src/main/java/CoBo/ChatbotAuth/Repository/ProfessorChatRepository.java
+++
@@ -1,8 +0,0 @@
-package CoBo.ChatbotAuth.Repository;
-
-import CoBo.ChatbotAuth.Data.Entity.Embedded.ChatId;
-import CoBo.ChatbotAuth.Data.Entity.ProfessorChat;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfessorChatRepository extends JpaRepository<ProfessorChat, ChatId> {
-}
